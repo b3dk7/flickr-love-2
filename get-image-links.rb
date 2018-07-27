397c1d66d1821750e5241c438e700d9646dc1ef6
snoop <snoop@zoe.zoe>
2018-07-27 17:43:35 +0200

    image links code

--- get-image-links.rb
+++ get-image-links.rb
@@ -1,22 +1,33 @@
 require 'open-uri'
+
 require 'csv'
 
+require 'nokogiri'
 
 CSV.foreach(ARGV[0]) do |row|
-    doc = open(row[0]).read    
-    if doc.include? '"o":{"displayUrl":"'
-        
-        
-        pre_match = doc.match(/"o":{"displayUrl":".*,"key":"o"/).to_s
-        
-        
-        puts pre_match.sub(/",".*url":"/, ",").sub('"o":{"displayUrl":"','').sub('","key":"o"','').gsub("\\",'').gsub('//','http://')
-        #match = pre_match.match(/c1\..*_o.jpg/).to_s.gsub('\\','')
-        
-        
-        #not_found = doc.match(
-        
-        
+    begin
+        doc = open(row[0]).read    
+        if doc.include? '"o":{"displayUrl":"'
+            
+            
+            pre_match = doc.match(/"o":{"displayUrl":".*,"key":"o"/).to_s
+            
+            img = pre_match.sub(/",".*url":"/, ",").sub('"o":{"displayUrl":"','').sub('","key":"o"','').gsub("\\",'').gsub('//','http://')
+            
+            puts img
+            #match = pre_match.match(/c1\..*_o.jpg/).to_s.gsub('\\','')
+            
+            tags = doc.match(/"keywords":.*","title":/).to_s.sub('"keywords":','').sub(',"title":','')
+            
+            #ng = Nokogiri::HTML(doc)
+
+            #not_found = doc.match(
+            open('tags.csv', 'a') { |f|
+                f.puts File.basename(img) + ',' + tags
+            }
+            
+        end
+    rescue
     end
     
 
